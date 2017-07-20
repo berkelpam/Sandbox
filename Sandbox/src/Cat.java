f5d8e99dfca689fde5b4036f3874697517ad0854
berkelpam <paul.tools@icloud.com>
2017-07-20 15:12:26 +0200

    initial version sandbox with cat circle and helloworld

---
+++ Sandbox/src/Cat.java
@@ -0,0 +1,12 @@
+public class Cat
+{
+   String name; // String is a special reference type for describing strings
+   Cat(String catName)
+   {
+      name = catName;
+   }
+   String name()
+   {
+      return name;
+   }
+}
---
+++ Sandbox/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args)
+	   {
+	     // System.out.println("hello" +args[0]);
+		
+		Cat cat = new Cat("Garfield");
+		System.out.println(cat.name()); // Output: Garfield
+		
+	   }
+}
